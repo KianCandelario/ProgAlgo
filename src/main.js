37ec681fee0c951b5c4133c7cb42a40deab47094
Russel Custodio <russelcustodio49@gmail.com>
2024-06-11 15:31:14 +0800

    Added: routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,48 @@
 import { createApp } from 'vue'
-import './styles/style.css'
-import './styles/index.css'
-import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import './style.css'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import HomePage from './components/HomePage.vue'
+import About from './components/About.vue'
+import FCFS from './components/Algorithm/FCFS.vue'
+import SPF from './components/Algorithm/SPF.vue'
+import RR from './components/Algorithm/RR.vue'
+
+
+const routes = [
+    {
+        path: '/',
+        redirect: '/FCFS',
+        component: HomePage,
+        children:[
+            {
+                path: '/FCFS',
+                component: FCFS
+            },
+            {
+                path: '/SJF',
+                component: SPF
+            },
+            {
+                path: '/RR',
+                component: RR
+            },
+        ]
+    },
+    {
+        path: '/about',
+        component: About,
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+// Create Vue app instance and use router
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
